34c822a7bbe4d59f2b6b525a1aad856fd580bd41
akshit-mehra <akshitme03@gmail.com>
2023-04-02 13:56:39 +0530

    impelmenting backend for login and Signup

--- backend/index.js
+++ backend/index.js
@@ -1,79 +1,39 @@
- require("dotenv").config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+var cors = require('cors')
 
 const port = 3001;
 
-const OpenAIimport = require("openai");
-const { Configuration, OpenAIApi } = OpenAIimport;
-const configuration = new Configuration({
-  organization: "org-k2OyhHx9Va4OUTbSwhYY2FMz",
-  apiKey: process.env.API_KEY,
+const app = express();
+mongoose.set("strictQuery", false);
 
-});
+app.use(express.json());
+app.use(cors())
 
-const openai = new OpenAIApi(configuration);
+// connect to mongodb
 
-async function sendPrompt(prompt) {
-  console.log("working on it.....");
-
-  console.log({prompt});
-
-
-  const model = "gpt-3.5-turbo";
-  const messages = [
-    {
-      "role": "system",
-      "content": "You are a pro chef. Your job is to give people options and recipes on how to cook with the ingredients they have. Prefer Indian dishes.",
-    },
-    {
-      "role": "user",
-      "content": `${prompt}`,
-    },
-  ];
-
-  const comlpetion = await openai.createChatCompletion({
-    model,
-    messages,
-  });
-
-  
-  console.log("done");
-  console.log(comlpetion.data.choices[0].message.content);
-
-    return (comlpetion.data.choices[0].message.content);
-
-}
-
-// const response = await openai.listEngines();
-
-
-
-app.post("/", async function (req, res) {
-
-    const { message } =  req.body;
-    console.log("req.body is " );
-    console.log(req.body);
-    // console.log(message);
-
-    // const message = "egg , spinach , onion";
-    const response = await sendPrompt(message);
-
-
-    if(response)
-    {
-        res.json({
-            message: response,
-          });
+//  using mongoose@6.10.0 temporarily to fix error - https://stackoverflow.com/a/75638135
+mongoose.connect(
+  "mongodb://localhost:27017/reciperoulette",
+  {
+    useNewUrlParser: true,
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to mongodb");
     }
+  }
+);
 
-});
+//   available routes
+app.use("/api/auth", require("./routes/auth"));
 
-app.listen(port, () => {
-  console.log("listening on port " + port);
+
+
+app.listen(port, function () {
+  console.log("listening on port" + port);
 });
---
+++ backend/tempindex.js
@@ -0,0 +1,79 @@
+//  require("dotenv").config();
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+
+// const port = 3001;
+
+// const OpenAIimport = require("openai");
+// const { Configuration, OpenAIApi } = OpenAIimport;
+// const configuration = new Configuration({
+//   organization: "org-k2OyhHx9Va4OUTbSwhYY2FMz",
+//   apiKey: process.env.API_KEY,
+
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// async function sendPrompt(prompt) {
+//   console.log("working on it.....");
+
+//   console.log({prompt});
+
+
+//   const model = "gpt-3.5-turbo";
+//   const messages = [
+//     {
+//       "role": "system",
+//       "content": "You are a pro chef. Your job is to give people options and recipes on how to cook with the ingredients they have. Prefer Indian dishes.",
+//     },
+//     {
+//       "role": "user",
+//       "content": `${prompt}`,
+//     },
+//   ];
+
+//   const comlpetion = await openai.createChatCompletion({
+//     model,
+//     messages,
+//   });
+
+  
+//   console.log("done");
+//   console.log(comlpetion.data.choices[0].message.content);
+
+//     return (comlpetion.data.choices[0].message.content);
+
+// }
+
+// // const response = await openai.listEngines();
+
+
+
+// app.post("/", async function (req, res) {
+
+//     const { message } =  req.body;
+//     console.log("req.body is " );
+//     console.log(req.body);
+//     // console.log(message);
+
+//     // const message = "egg , spinach , onion";
+//     const response = await sendPrompt(message);
+
+
+//     if(response)
+//     {
+//         res.json({
+//             message: response,
+//           });
+//     }
+
+// });
+
+// app.listen(port, () => {
+//   console.log("listening on port " + port);
+// });
