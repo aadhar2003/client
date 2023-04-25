126b61ca5618170ea667ef48fe3fa73fe62e4a5d
Aadhar <aadhargoel3@gmail.com>
2023-04-25 20:26:36 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Nav/>
-      {/* <Home/> */}
-      <Gptpage/>
+      <Home/>
+      {/* <Gptpage/> */}
       <Footer/>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from '../About/About'
 import Trendingsection from '../Trending/Trendingsection'
+import FindRecipies from '../FindRecipies/FindRecipies'
 
 function Home() {
   return (
     <>
        <About/>
       <Trendingsection/>
+      <FindRecipies/>
     </>
   )
 }
